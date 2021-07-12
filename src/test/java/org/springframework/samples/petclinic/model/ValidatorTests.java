b0b4f57d8efd9c27f059d03e5d601d73d6be64e2
berkd <berkd@berkds-MacBook-Pro.local>
2021-07-12 12:32:02 +0300

    Test fail exercise

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("TEXT TO FAIL TEST CASE");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
